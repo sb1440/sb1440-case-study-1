71734d7aae7ec8d605b0b4ee4aeade08febcf98c
sb1440 <sankarbareddy@gmail.com>
2022-03-24 21:10:32 +0530

    Initial commit

---
+++ model.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+
+app = Flask(__name__)
+
+with open('classifier.pkl', 'rb') as clf:
+    classifier = pickle.load(clf)
+        
+def preprocess(txt):
+    ''' Transorms the input as per the requirement of the model'''
+     
+    input_file = pd.read_csv(txt, header=None)  
+    input_file = input_file.replace('na', np.NaN)                
+    
+    
+    with open('Discard_Features.pkl', 'rb') as df:
+        discard_features = pickle.load(df)
+    input_file = input_file.drop(discard_features, axis =1)
+    
+    with open('Imputer.pkl', 'rb') as imp:
+        knn_imp = pickle.load(imp)
+    test_imp = knn_imp.transform(input_file)
+    
+    with open('standardizer.pkl', 'rb') as scale:
+        std = pickle.load(scale)
+    test_std = std.transform(test_imp)
+    
+    with open('PCA_features.pkl', 'rb') as fe:
+        pca = pickle.load(fe)
+    test_80 = pca.transform(test_std)
+    
+    return test_80
+
+
+
+
+@app.route('/')
+def man():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    vehicle = request.files['file']
+    instance = preprocess(vehicle)
+    pred = classifier.predict(instance)
+    return render_template('result.html', data=pred)
+        
+#         prediction = "Positive - APS may be in bad condition, manual inspection is required" 
+#     else:
+#         prediction = "Negative - APS is in fine condition"
+
+#     return jsonify({'prediction': prediction})
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
